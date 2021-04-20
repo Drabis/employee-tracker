95d4357caee9a4f568fc74c873e81f0a6fef6efc
Drabis <drabis47@gmail.com>
2021-04-19 21:01:10 -0400

    display.js and all package installation

---
+++ DisplayMess.js
@@ -0,0 +1,30 @@
+const figlet = require('figlet');
+const term = require("terminal-kit").terminal;
+
+const start = () => {
+  console.log("\n");
+  term.cyan(figlet.textSync('\nEmployee\n Tracker', {
+    font: 'Big Money-ne',
+    horizontalLayout: 'full',
+    width: 100,
+    whitespaceBreak: true
+  }));
+
+  console.log("\n");
+};
+
+const exit = () => {
+  console.log("\n");
+  term.yellow(figlet.textSync('Thank You', {
+    font: 'Ghost',
+    horizontalLayout: 'full',
+    width: 100,
+    whitespaceBreak: true
+  }));
+
+  console.log("\n");
+}
+
+
+// EXPORT MODULE 
+module.exports = { start, exit};
